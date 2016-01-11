48b6d677071ccec271a4218044ee524f3b3d713a
gogo <376830949@qq.com>
2016-01-11 18:41:25 +0800

    修改路由结构

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,30 @@
 import 'babel-core/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { Router } from 'react-router';
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { Router, Route } from 'react-router';
 import { Provider } from 'react-redux';
 import { ReduxRouter } from 'redux-router';
 import configureStore from './store/configureStore';
-import routes from './common/routes';
 
-const history = createBrowserHistory();
+import App from './containers/App';
+import Login from './containers/Login';
+
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(initialState);
 
 //引入样式
-import "../css/index.css";
+import '../css/index.css';
 
 //挂载点
 const mountId = document.getElementById('root');
 //渲染
-render(
-    <Provider store={store} >
-        <Router children={routes} history={history} />
-    </Provider>,
-    mountId
-);
+render((
+	<Provider store={store} >
+	  	<Router>
+	  		<Route path="/">
+		    	<Route path="login" component={ Login } />
+		      	<Route path="app" component={ App } />
+		    </Route>
+	  	</Router>
+	</Provider>
+), mountId)
