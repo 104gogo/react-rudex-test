0ed4326fa0b0fe75dd456c7b10b72a9282fd6f47
gogo <376830949@qq.com>
2016-02-17 16:43:52 +0800

    添加cookie

--- src/js/actions/login.js
+++ src/js/actions/login.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch';
+import { setCookie, getCookieValue } from '../lib/cookie';
 
 export const LOGIN = 'LOGIN';
 export const CHANGE_USERNAME = 'CHANGE_USERNAME';
@@ -8,14 +9,23 @@ export function login() {
     return (dispatch, getState) => {
     	const { username, password } = getState().login.toJS();
 
-		return fetch(`http://192.168.1.51:8671/login/${ username }/post`, {
+		return fetch(`http://192.168.1.51:18671/login/${ username }/post`, {
 		  	method: 'post',
 		  	headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 		  	body: `password=${ password }&x-dms-request-id=${ new Date().getTime() }`
 		})
 		.then(res => res.json())
-		.then(json => {
-			location.hash = '#index';
+		.then(json => {	
+			const { uuid, sid, userName, roleCode } = json.content;
+
+			setCookie('dmsUserEmail', username);
+			setCookie('dmsPassWord', password);	
+			setCookie('dmsUuid', uuid);
+			setCookie('dmsSid', sid);
+			setCookie('dmsUserName', userName);
+			setCookie('dmsRoleCode', roleCode);
+
+			location.hash = '#app/index';
 		});
 	};
 }
