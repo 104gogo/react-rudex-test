c3b1c1384432c4a589c05ddda3babc48b906e300
gogo <376830949@qq.com>
2016-02-18 17:28:39 +0800

    添加Index组件

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -14,6 +14,7 @@ class App extends Component {
             <div>
                 <Header header={ header } action={ actions.clickUserName } />
                 <Menu />
+                { this.props.children }
             </div>
         )
     }
--- src/app/containers/Index.js
+++ src/app/containers/Index.js
@@ -4,11 +4,39 @@ import { connect } from 'react-redux';
 
 class Index extends Component {
     render() {
-        
+       
         return (
-            <div>123
-               
-            </div>
+        	<div id="container">
+				<div className="content index mbSix">
+					<div className="block-box fl">
+						<div className="apiType-title title">
+							<div className="fl">
+								<input id="js_searchInput" className="default-border" placeholder="输入关键字" />
+								<button id="js_searchBtn" className="green-btn search-btn">查询</button>
+								<a href="javascript:;" id="js_about_me" className="about_me">我负责的</a>
+							</div>
+							
+							<a href="javascript:;" className="js_toAddProduct smallAddBtn" className="fr">添加产品</a>
+							<a href="javascript:;" className="js_toAddVersion smallAddBtn" className="fr">添加版本</a>
+							<a href="javascript:;" className="js_toAddApi smallAddBtn" className="fr">添加API</a>
+							
+						</div>
+						<div className="apiType-content block-content">
+							<div id="js_index_table"></div>
+							<div id="js_page"></div>
+						</div>
+						
+					</div>
+					<div className="fr news">
+						<div id="js_submitAudit" className="index-info mine block-box">
+							
+						</div>
+						<div id="js_waitAudit" className="index-info latest block-box">
+					
+						</div>
+					</div>
+				</div>
+			</div>
         )
     }
 }  
