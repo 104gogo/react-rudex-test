e10b3a5f5c18c06b435ee7884c77c8378c6d7abf
gogo <376830949@qq.com>
2016-02-18 17:12:05 +0800

    up

--- src/app/components/index/Menu.js
+++ src/app/components/index/Menu.js
@@ -4,21 +4,36 @@ class Menu extends Component {
 	render() {
 		return <div className="menu-list">
 			<ul>
-				<a href="#index"><li className="home header_active"><span className="home-active"></span>
-					<div className="longen"> <label></label>首页</div>
-				</li></a>
-				<a href="#apiManage"><li className="manage"><span className=""></span>
-				<div className="longen"> <label></label>API管理</div>
-				</li></a>
-				<a href="#mock/mockManage"><li className="mock"><span className=""></span>
-				<div className="longen"> <label></label>mock</div>
-				</li></a>
-				<a href="#test/testCase"><li className="test"><span className=""></span>
-				<div className="longen"> <label></label>test</div>
-				</li></a>
-				<a href="#system/setApi"><li className="set"><span className=""></span>
-				<div className="longen"> <label></label>设置</div>
-				</li></a>
+				<a href="#index">
+					<li className="home header_active">
+						<span className="home-active"></span>
+						<div className="longen"><label></label><p>首页</p></div>
+					</li>
+				</a>
+				<a href="#apiManage">
+					<li className="manage">
+						<span className=""></span>
+						<div className="longen"><label></label><p>API管理</p></div>
+					</li>
+				</a>
+				<a href="#mock/mockManage">
+					<li className="mock">
+						<span className=""></span>
+						<div className="longen"><label></label><p>mock</p></div>
+					</li>
+				</a>
+				<a href="#test/testCase">
+					<li className="test">
+						<span className=""></span>
+						<div className="longen"><label></label><p>test</p></div>
+					</li>
+				</a>
+				<a href="#system/setApi">
+					<li className="set">
+						<span className=""></span>
+						<div className="longen"><label></label><p>设置</p></div>
+					</li>
+				</a>
 			</ul>
 		</div> 
 	}
