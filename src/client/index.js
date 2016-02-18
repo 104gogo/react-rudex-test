be6e3ace4efeb91e69fe1b1d510aed9704bbf8fb
gogo <376830949@qq.com>
2016-02-18 13:59:02 +0800

    更新react-router新写法，2.0

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
 import { Provider } from 'react-redux';
+import { createHashHistory } from 'history';
 import configureStore from '../app/store/configureStore';
 import routes from '../app/routes';
 
+//引入样式
+// import '../../public/css/index.css';
+
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(initialState);
+const history = useRouterHistory(createHashHistory)({ queryKey: false });
 
 render((
 	<Provider store={ store } >
-	  	<Router>
-	  		{ routes }
-	  	</Router>
+	  	<Router history={ history } routes={ routes } />
 	</Provider>
 ), document.getElementById('root'));
